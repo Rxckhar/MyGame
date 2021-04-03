8c29ec5482a75b9dd98772e14e859aaaf70a1975
Сафин Руслан Мухсиддинович <pl8426@C201N8.pl1.edu>
2021-04-03 11:56:14 +0500

    save

---
+++ main.js
@@ -0,0 +1,23 @@
+let cube = {
+    id: 'cube',
+    x: 0,
+    y: 0,
+    width: 56,
+    height: 56,
+    trnstn: .25,
+    step: 56,
+    color: 'black'
+}
+
+let map = {
+    x: 0,
+    y: 0,
+    width: 470,
+    height: 470,
+    color: '#f5f5f5',
+    border: '#f1f1f1'
+}
+
+renderMap(map);
+
+renderCube(cube);
